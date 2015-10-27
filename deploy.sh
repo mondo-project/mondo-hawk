d6515ec0679dfe0a08ce7e3ebd6587d1f70ee8eb
Antonio García-Domínguez <nyoescape@gmail.com>
2015-10-27 14:01:25 +0000

    Add automatic push of update site through Travis

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Run the regular build
+mvn install
+
+# Clone the gh-pages branch
+rm -rf out || true
+git clone -b gh-pages --single-branch https://github.com/mondo-project/mondo-hawk.git out
+cd out
+
+# Indicate clearly that this commit comes from Travis
+git config user.name "Travis CI"
+git config user.email "antonio.garciadominguez@york.ac.uk"
+
+# If the tip comes from Travis, amend it. Otherwise, add a new commit.
+rm -rf hawk-updates
+cp -r ../org.hawk.updatesite/target/repository hawk-updates
+git add .
+if git log --format=%an HEAD~.. | grep -q "Travis CI"; then
+    COMMIT_FLAGS="--amend"
+fi
+git commit $COMMIT_FLAGS -am "Build update site"
+
+# Force push to the gh-pages branch
+git push --force "https://${GH_TOKEN}@${GH_REF}" gh-pages
