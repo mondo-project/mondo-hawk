c19d23f4f983bb79cce642a92dac7d747a0e128a
Antonio García-Domínguez <nyoescape@gmail.com>
2015-12-15 15:44:35 +0000

    exml.test: add ModelioObjectTest

---
+++ org.hawk.modelio.exml.tests/src/org/hawk/modelio/exml/model/ModelioObjectTest.java
@@ -0,0 +1,36 @@
+package org.hawk.modelio.exml.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Map;
+
+import org.hawk.modelio.exml.metamodel.ModelioAttribute;
+import org.hawk.modelio.exml.metamodel.ModelioClass;
+import org.hawk.modelio.exml.metamodel.ModelioMetaModelResource;
+import org.hawk.modelio.exml.parser.ExmlObject;
+import org.hawk.modelio.exml.parser.ExmlParser;
+import org.junit.Test;
+
+public class ModelioObjectTest {
+
+	private static final String FRAGMENT_PATH = "resources/Zoo/data/fragments/";
+	private static final String CLASSDIAG_EXML = FRAGMENT_PATH + "Zoo/model/ClassDiagram/cf6a3b18-94f9-49ba-b8d9-653cb2f93cfb.exml";
+
+	@Test
+	public void accessClassDiagram() throws Exception {
+		final File f = new File(CLASSDIAG_EXML);
+		try (final FileInputStream fIS = new FileInputStream(f)) {
+			final ExmlParser parser = new ExmlParser();
+			final ExmlObject object = parser.getObject(f, fIS);
+			final ModelioObject mO = new ModelioObject(new ModelioMetaModelResource(null), object);
+			final ModelioClass mC = (ModelioClass) mO.getType();
+
+			final Map<String, ModelioAttribute> attrs = mC.getAttributes();
+			assertTrue(((String)mO.get(attrs.get("UiData"))).startsWith("eJztXWtz27"));
+			assertEquals(88, mO.get(attrs.get("UiDataVersion")));
+		}
+	}
+}
