04af44278213d2a86cdf8c1d2bf1b20b62cc69f0
Antonio García-Domínguez <nyoescape@gmail.com>
2016-06-16 10:43:25 +0100

    deploy.sh: only deploy with non-PR commits to master branch

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
+deploy_updates() {
+    # Clone the gh-pages branch
+    rm -rf out || true
+    git clone -b gh-pages --single-branch https://github.com/mondo-project/mondo-hawk.git out
+    cd out
+
+    # Indicate clearly that this commit comes from Travis
+    git config user.name "Travis CI"
+    git config user.email "antonio.garciadominguez@york.ac.uk"
+
+    # If the tip comes from Travis, amend it. Otherwise, add a new commit.
+    rm -rf hawk-updates
+    cp -r ../org.hawk.updatesite/target/repository hawk-updates
+    git add --all .
+    if git log --format=%an HEAD~.. | grep -q "Travis CI"; then
+	COMMIT_FLAGS="--amend"
+    fi
+    git commit $COMMIT_FLAGS -am "Build update site"
+
+    # Force push to the gh-pages branch
+    git push --force "https://${GH_TOKEN}@${GH_REF}" gh-pages &>/dev/null
+}
+
 # Exit immediately if something goes wrong
 set -o errexit
 
 # Run the regular build
 mvn --quiet install
 
-# Only continue deploying to update site for the master branch
-if [[ "$TRAVIS_BRANCH" != 'master' ]]; then
-    exit 0
+# Only continue deploying to update site for non-PR commits to the master branch
+if [[ "$TRAVIS_BRANCH" == 'master' && "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
+    deploy_updates
 fi
-
-# Clone the gh-pages branch
-rm -rf out || true
-git clone -b gh-pages --single-branch https://github.com/mondo-project/mondo-hawk.git out
-cd out
-
-# Indicate clearly that this commit comes from Travis
-git config user.name "Travis CI"
-git config user.email "antonio.garciadominguez@york.ac.uk"
-
-# If the tip comes from Travis, amend it. Otherwise, add a new commit.
-rm -rf hawk-updates
-cp -r ../org.hawk.updatesite/target/repository hawk-updates
-git add --all .
-if git log --format=%an HEAD~.. | grep -q "Travis CI"; then
-    COMMIT_FLAGS="--amend"
-fi
-git commit $COMMIT_FLAGS -am "Build update site"
-
-# Force push to the gh-pages branch
-git push --force "https://${GH_TOKEN}@${GH_REF}" gh-pages &>/dev/null
