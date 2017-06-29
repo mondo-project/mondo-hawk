d41b3ef951b48e3df6c8b158056a16beb5dc54d6
orjuwan Al-Wadeai <orjwanati@hotmail.com>
2017-06-29 12:24:07 +0100

    Enum Type for aggregation

---
+++ plugins/org.hawk.modelio.exml/src/org/hawk/modelio/metamodel/parser/MAggregationType.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Aston University
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Orjuwan Al-Wadeai - 
+ ******************************************************************************/
+package org.hawk.modelio.metamodel.parser;
+
+public enum MAggregationType {
+	None,
+    Composition,
+    SharedAggregation;
+}
+
