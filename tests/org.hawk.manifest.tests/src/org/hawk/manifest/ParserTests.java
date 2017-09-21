749b5b9d6bb88b2b8d9bc1868828f8abf85ad256
Antonio García-Domínguez <nyoescape@gmail.com>
2017-09-21 18:59:53 -0500

    manifest.tests: starting test for Eclipse manifest parser

---
+++ tests/org.hawk.manifest.tests/src/org/hawk/manifest/ParserTests.java
@@ -0,0 +1,34 @@
+package org.hawk.manifest;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hawk.core.model.IHawkObject;
+import org.hawk.manifest.model.ManifestModelResource;
+import org.hawk.manifest.model.ManifestModelResourceFactory;
+import org.junit.Test;
+
+public class ParserTests {
+
+	@Test
+	public void testDuplicateDependencies() {
+		ManifestModelResourceFactory rFactory = new ManifestModelResourceFactory();
+		ManifestModelResource resource = rFactory.parse(null, new File("resources/dupdep/META-INF/MANIFEST.MF"));
+
+		final Map<String, Integer> instancesByType = new HashMap<>();
+		for (IHawkObject obj : resource.getAllContents()) {
+			final String typeName = obj.getType().getName();
+
+			Integer oldCount = instancesByType.get(typeName);
+			instancesByType.put(typeName, oldCount == null ? 1 : oldCount + 1);
+		}
+
+		// 1 from the manifest itself + 1 per require-bundle (even if it's duplicate)
+		assertEquals(3, (int) instancesByType.get("ManifestBundle"));
+		assertEquals(2, (int) instancesByType.get("ManifestRequires"));
+		assertEquals(1, (int) instancesByType.get("ManifestBundleInstance"));
+	}
+}
