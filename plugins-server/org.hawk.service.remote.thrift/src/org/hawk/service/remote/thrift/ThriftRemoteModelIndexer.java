0ee0866844ad9debbdcbbb1096372e9d699f117e
Antonio García-Domínguez <nyoescape@gmail.com>
2017-11-21 18:57:12 +0000

    thrift: minor import cleanup

--- plugins-server/org.hawk.service.remote.thrift/src/org/hawk/service/remote/thrift/ThriftRemoteModelIndexer.java
+++ plugins-server/org.hawk.service.remote.thrift/src/org/hawk/service/remote/thrift/ThriftRemoteModelIndexer.java
@@ -26,7 +26,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TimerTask;
 
 import org.apache.activemq.artemis.api.core.client.ClientMessage;
 import org.apache.activemq.artemis.api.core.client.MessageHandler;
