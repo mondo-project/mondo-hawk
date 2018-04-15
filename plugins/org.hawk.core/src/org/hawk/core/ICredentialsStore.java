06a170f4250950958b6c4e8c58dc4dd19f15cdea
Antonio García-Domínguez <nyoescape@gmail.com>
2018-04-15 20:01:32 +0100

    Add hashCode/equals to Credentials

--- plugins/org.hawk.core/src/org/hawk/core/ICredentialsStore.java
+++ plugins/org.hawk.core/src/org/hawk/core/ICredentialsStore.java
@@ -54,6 +54,37 @@ public interface ICredentialsStore {
 		public String getPassword() {
 			return password;
 		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((password == null) ? 0 : password.hashCode());
+			result = prime * result + ((username == null) ? 0 : username.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Credentials other = (Credentials) obj;
+			if (password == null) {
+				if (other.password != null)
+					return false;
+			} else if (!password.equals(other.password))
+				return false;
+			if (username == null) {
+				if (other.username != null)
+					return false;
+			} else if (!username.equals(other.username))
+				return false;
+			return true;
+		}
 	}
 
 	/**
