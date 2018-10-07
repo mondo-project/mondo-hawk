735dc32ace5415d6f1782c22353f0ac9ed2e5812
Antonio García-Domínguez <nyoescape@gmail.com>
2018-10-07 03:17:28 +0100

    server.gzip: update to Jetty 9.4

--- plugins-server/org.hawk.service.server.gzip/src/org/hawk/service/server/gzip/Customizer.java
+++ plugins-server/org.hawk.service.server.gzip/src/org/hawk/service/server/gzip/Customizer.java
@@ -16,75 +16,25 @@
  *******************************************************************************/
 package org.hawk.service.server.gzip;
 
-import java.io.IOException;
-import java.util.Collections;
 import java.util.Dictionary;
-import java.util.Set;
 import java.util.zip.Deflater;
-import java.util.zip.DeflaterOutputStream;
-import java.util.zip.GZIPOutputStream;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.equinox.http.jetty.JettyCustomizer;
 import org.eclipse.jetty.server.handler.ContextHandler;
-import org.eclipse.jetty.servlets.gzip.AbstractCompressedStream;
-import org.eclipse.jetty.servlets.gzip.CompressedResponseWrapper;
-import org.eclipse.jetty.servlets.gzip.GzipHandler;
+import org.eclipse.jetty.server.handler.gzip.GzipHandler;
 
 public class Customizer extends JettyCustomizer {
-
-	private static final class CustomLevelGzipHandler extends GzipHandler {
-
-		private final int level;
-
-		public CustomLevelGzipHandler(int level) {
-			this.level = level;
-		}
-
-		@Override
-		protected CompressedResponseWrapper newGzipResponseWrapper(HttpServletRequest request, HttpServletResponse response) {
-			// This is pretty much a straight copy of the original version, but using a slightly different GZIPOutputStream
-			return new CompressedResponseWrapper(request,response)
-		    {
-		        {
-		            super.setMimeTypes(getMimeTypes(), _excludeMimeTypes);
-		            super.setBufferSize(getBufferSize());
-		            super.setMinCompressSize(getMinGzipSize());
-		        }
-		        
-		        @Override
-		        protected AbstractCompressedStream newCompressedStream(HttpServletRequest request,HttpServletResponse response) throws IOException
-		        {
-		            return new AbstractCompressedStream("gzip",request,this,_vary)
-		            {
-		                @Override
-		                protected DeflaterOutputStream createStream() throws IOException
-		                {
-		                	return new GZIPOutputStream(_response.getOutputStream(), _bufferSize){{def.setLevel(level);}};			                    	
-		                }
-		            };
-		        }
-		    };
-		}
-
-		@Override
-		public Set<String> getMimeTypes() {
-			// MIME types to be compressed
-			return Collections.singleton("application/x-thrift");
-		}
-
-	}
 
 	@Override
 	public Object customizeContext(Object context, Dictionary<String, ?> settings) {
 		if (context instanceof ContextHandler) {
-			final GzipHandler gzipHandler = new CustomLevelGzipHandler(Deflater.BEST_COMPRESSION);
-			gzipHandler.setBufferSize(16384);
+			final GzipHandler gzipHandler = new GzipHandler();
+			gzipHandler.setCompressionLevel(Deflater.BEST_COMPRESSION);
+			gzipHandler.addIncludedMimeTypes("application/x-thrift");
 			final ContextHandler contextHandler = (ContextHandler)context;
-			contextHandler.setHandler(gzipHandler);
+			contextHandler.insertHandler(gzipHandler);
 		}
 		return super.customizeContext(context, settings);
 	}
+
 }
