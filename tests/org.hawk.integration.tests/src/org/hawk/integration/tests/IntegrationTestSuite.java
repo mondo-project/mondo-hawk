bde019019511148b16b82ca2c4a62e09ad4c98ef
Antonio García-Domínguez <nyoescape@gmail.com>
2018-11-11 19:13:40 +0000

    IntegrationTestSuite: update with newest test classes

--- tests/org.hawk.integration.tests/src/org/hawk/integration/tests/IntegrationTestSuite.java
+++ tests/org.hawk.integration.tests/src/org/hawk/integration/tests/IntegrationTestSuite.java
@@ -20,11 +20,15 @@ import org.hawk.backend.tests.BackendTestSuite;
 import org.hawk.backend.tests.factories.IGraphDatabaseFactory;
 import org.hawk.integration.tests.bpmn.ModelVersioningTest;
 import org.hawk.integration.tests.emf.DerivedFeatureTest;
+import org.hawk.integration.tests.emf.DerivedFromMetaPropertiesTest;
+import org.hawk.integration.tests.emf.MetamodelQueryTest;
 import org.hawk.integration.tests.emf.ScopedQueryTest;
+import org.hawk.integration.tests.emf.SubtreeContextTest;
 import org.hawk.integration.tests.emf.CountInstancesTest;
 import org.hawk.integration.tests.emf.TreeUpdateTest;
 import org.hawk.integration.tests.manifests.ManifestIndexQueryTest;
 import org.hawk.integration.tests.modelio.ModelioMetamodelPopulationTest;
+import org.hawk.integration.tests.modelio.ModelioProxyResolutionTest;
 import org.hawk.integration.tests.uml.UMLIndexingTest;
 import org.hawk.integration.tests.uml.UMLWorkspaceIndexingTest;
 import org.junit.runner.RunWith;
@@ -38,10 +42,14 @@ import com.github.peterwippermann.junit4.parameterizedsuite.ParameterizedSuite;
 @SuiteClasses({
 	CountInstancesTest.class,
 	DerivedFeatureTest.class,
+	DerivedFromMetaPropertiesTest.class,
 	ManifestIndexQueryTest.class,
+	MetamodelQueryTest.class,
+	ModelioProxyResolutionTest.class,
 	ModelioMetamodelPopulationTest.class,
 	ModelVersioningTest.class,
 	ScopedQueryTest.class,
+	SubtreeContextTest.class,
 	TreeUpdateTest.class,
 	UMLIndexingTest.class,
 	UMLWorkspaceIndexingTest.class,
