5c653b308314dcb8b80f4f2f024a67b93d9bd0e5
Antonio García-Domínguez <nyoescape@gmail.com>
2019-02-15 09:44:33 +0000

    deploy.sh: use Bintray for client products and API libraries as well

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,37 @@
 
 # Retrieve major+minor version from the core MANIFEST.MF, keep patch level at 0
 get_version() {
-    grep Bundle-Version ../plugins/org.hawk.core/META-INF/MANIFEST.MF \
+    grep Bundle-Version plugins/org.hawk.core/META-INF/MANIFEST.MF \
 	      | cut --delim=: -f2 \
 	      | sed -re 's/ *([0-9]+)[.]([0-9]+)[.].*/\1.\2.0/'
 }
 
+# Usage: bintray METHOD PATH ARGS...
+#
+# PATH should start with a slash.
+# METHOD should be GET, POST, PUT, DELETE...
+bintray() {
+    METHOD="$1"
+    PATH="$2"
+    shift 2
+
+    curl -X "$METHOD" -u "$BINTRAY_API_USER:$BINTRAY_API_KEY" \
+         "$@" "https://api.bintray.com$PATH" > /dev/null 2>&1 \
+        || echo "$METHOD invocation to $PATH failed"
+}
+
+delete_version() {
+    if test -n "$1"; then
+        bintray DELETE "/packages/$BINTRAY_API_USER/generic/hawk/versions/$1"
+    else
+        echo "Missing version in delete_version"
+        return 1
+    fi
+}
+
 upload_to_bintray() {
-    VERSION=$(get_version)
     for f in "$@"; do
-        curl -X PUT -T "$f" -u "$BINTRAY_API_USER:$BINTRAY_API_KEY" \
-             "https://api.bintray.com/content/$BINTRAY_API_USER/generic/hawk/$VERSION/$(basename "$f");publish=1;override=1" \
-            || echo "upload failed"
+        bintray PUT "/content/$BINTRAY_API_USER/generic/hawk/$VERSION/$(basename "$f");publish=1;override=1" -T "$f"
     done
 }
 
@@ -26,14 +46,9 @@ deploy_updates() {
     git config user.name "Travis CI"
     git config user.email "agarcdomi@gmail.com"
 
-    VERSION=$(get_version)
-    echo "Detected version $VERSION of Hawk"
-
     # If the tip comes from Travis, amend it. Otherwise, add a new commit.
     rm -rf hawk-updates/${VERSION}
     cp -r ../releng/org.hawk.updatesite/target/repository hawk-updates/${VERSION}
-    tar czf hawk-thrift-js-${VERSION}.tar.gz ../plugins-server/org.hawk.service.api/src-gen-js
-    tar czf hawk-thrift-cpp-${VERSION}.tar.gz ../plugins-server/org.hawk.service.api/src-gen-cpp
     git add --all .
     if git log --format=%an HEAD~.. | grep -q "Travis CI"; then
 	      COMMIT_FLAGS="--amend"
@@ -48,17 +63,29 @@ deploy_updates() {
 
 deploy_products() {
     pushd out
+
+    delete_version "$VERSION"
     upload_to_bintray ../releng/org.hawk.service.server.product/target/products/hawk-server-nogpl_*.zip
+    upload_to_bintray ../releng/org.hawk.service.cli.product/target/products/hawk-cli_*.zip
+
+    tar czf "../hawk-thrift-js-${VERSION}.tar.gz" ../plugins-server/org.hawk.service.api/src-gen-js
+    tar czf "../hawk-thrift-cpp-${VERSION}.tar.gz" ../plugins-server/org.hawk.service.api/src-gen-cpp
+    upload_to_bintray ../hawk-thrift-*.tar.gz
     popd
 }
 
 # Exit immediately if something goes wrong
 set -o errexit
 
+# Find out the current version of Hawk
+export VERSION=$(get_version)
+echo "Detected version $VERSION of Hawk"
+
 # Run the regular build, skip tests (these were run in a previous stage)
 mvn --quiet clean install -DskipTests
 
 # Only continue deploying to update site for non-PR commits to the master branch
 if [[ "$TRAVIS_BRANCH" == 'master' && "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
-    deploy_updates && deploy_products
+    deploy_updates
+    deploy_products
 fi
